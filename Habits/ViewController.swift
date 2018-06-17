a2e5cdccdbf975364871db8433b3289d9163aa21
William <williamjrobinson@me.com>
2018-06-17 16:20:04 +0100

    Initial Commit

---
+++ Habits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Habits
+//
+//  Created by William Robinson on 17/06/2018.
+//  Copyright © 2018 William Robinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
